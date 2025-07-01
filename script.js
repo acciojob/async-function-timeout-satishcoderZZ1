ce9c70b677ca41f3e7b8813111b867365c6cbb20
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-07-01 12:41:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function wait(ms){
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function showMessage(){
+	const text = document.getElementById("text").value;
+	const delay = parseInt(document.getElementById("delay").value);
+	const outputDiv = document.getElementById("output");
+
+	outputDiv.innerText = "";
+	await wait(delay);
+	outputDiv.innerText = text;
+}
+
+document.getElementById("btn").addEventListener("click", showMessage);
